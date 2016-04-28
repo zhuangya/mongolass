5ec89f962e4bb76dc37c94d6b070cfbce549f50e
nswbmw <gxqzk@126.com>
2016-04-28 15:39:47 +0800

    add Types & bump v1.1.0

---
+++ test/Types.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const MONGODB = process.env.MONGODB || 'mongodb://localhost:27017/test';
+
+let assert = require('assert');
+let Mongolass = require('..');
+let Schema = Mongolass.Schema;
+let Types = Mongolass.Types;
+let mongolass = new Mongolass(MONGODB);
+
+let UserSchema = new Schema('User', {
+  uid: { type: Types.ObjectId },
+});
+let User = mongolass.model('User', UserSchema);
+
+describe('Types.js', function () {
+  before(function* () {
+    yield User.insertOne({ uid: '5721bb5abec50ab84b8eb109' });
+  });
+
+  after(function* () {
+    yield User.remove();
+    mongolass.disconnect();
+  });
+
+  it('ObjectId wrong', function* () {
+    try {
+      yield User.insertOne({ uid: 'haha' });
+    } catch(e) {
+      assert.deepEqual(e.message, '($.uid: "haha") ✖ (type: ObjectId)');
+    }
+  });
+
+  it('ObjectId', function* () {
+    let user = yield User.findOne();
+    assert.ok('object'  === typeof user._id);
+    assert.deepEqual(user.uid.toString(), '5721bb5abec50ab84b8eb109');
+  });
+});
